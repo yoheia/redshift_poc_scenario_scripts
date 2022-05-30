9efa2320c221421f348b1262266c48a3d0ad990f
yoheia <yohei.az@gmail.com>
2022-05-30 23:42:31 +0000

    modified some scripts

--- redshift_poc_senario1.sh
+++ redshift_poc_senario1.sh
@@ -6,14 +6,14 @@ CURRENT_DATE=`date '+%Y-%m-%d-%H%M%S'`
 BASE_DIR=$(cd $(dirname $0);pwd)
 cd $BASE_DIR
 
-export PGBENCH_PATH=pgbench
+PGBENCH_PATH=${PGBENCH_PATH:-/usr/pgsql-13/bin/pgbench}
 export PG_HOST=redshift-cluster-poc.ceyg6jv96hfq.ap-northeast-1.redshift.amazonaws.com
 
 # 定期的にシステムビューを S3 に UNLOAD する
-nohup ./redshift_polling_unload/polling_unload.sh &
+nohup ../redshift_query_executer/redshift_query_executer.sh &
 
 # スループット検証実行
-nohup ./redshift_concurrent_query_executer/executer/redshift_concurrent_workload_awsampledb.sh &
+nohup ../redshift_concurrent_query_executer/executer/redshift_concurrent_workload_awsampledb.sh &
 
 
 exit 0
